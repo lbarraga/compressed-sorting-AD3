86166c7ef2f6591ee0d3000d33561544d589a018
lbarraga <lukas.barragantorres@ugent.be>
2023-10-14 23:18:38 +0200

    fill frequency table

--- src/tree/tree.c
+++ src/tree/tree.c
@@ -1,3 +1,34 @@
 //
 // Created by lukasbt on 10/14/23.
 //
+
+#include <stdio.h>
+
+void fillFrequencyTable(FILE* file, int* frequencyTable) {
+    int ch = fgetc(file);
+    while (ch != EOF) {
+        frequencyTable[ch]++;
+        ch = fgetc(file);
+    }
+}
+
+void tree(const char *inputFilePath, const char *outputFilePath, int bufferSize) {
+    int MAX_CHAR_COUNT = 128;
+
+    int frequencyTable[MAX_CHAR_COUNT];
+
+    for (int i = 0; i < MAX_CHAR_COUNT; i++) {
+        frequencyTable[i] = 0;
+    }
+
+    FILE* inputFile = fopen(inputFilePath, "r");
+    FILE* outputFile = fopen(outputFilePath, "w");
+
+    fillFrequencyTable(inputFile, frequencyTable);
+
+    for (int i = 0; i < MAX_CHAR_COUNT; i++) {
+        if (frequencyTable[i] != 0) {
+            fprintf(outputFile, "%d %d\n", i, frequencyTable[i]);
+        }
+    }
+}
--- src/tree/tree.h
+++ src/tree/tree.h
@@ -5,6 +5,16 @@
 #ifndef PROJECTAD3_TREE_H
 #define PROJECTAD3_TREE_H
 
+/**
+ * Tel hoeveel keer elk karakter voorkomt in een bestand, bereken de optimale orde-bewarende prefix-codering (OPC) en
+ * schrijf deze uit naar een bestand, volgens volgend formaat:
+ *
+ * \<karakter-waarde (decimaal)\> \<aantal voorkomens (decimaal)\> \<orde-bewarende prefix-code (binair)\>
+ *
+ * @param inputFile Het pad naar het inputbestand.
+ * @param outputFile Het pad naar het outputbestand.
+ * @param bufferSize De grootte van de buffer die je mag gebruiken om de ingelezen strings in op te slaan, in bytes.
+ */
 void tree(const char *inputFile, const char *outputFile, int bufferSize);
 
 #endif //PROJECTAD3_TREE_H
