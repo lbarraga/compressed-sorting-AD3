440d9e11d58e67803c871cf3700e2c5fde0b7256
Rien Maertens <rien.maertens@posteo.be>
2023-11-20 15:17:56 +0100

    Extra verduidelijking, extra testbestand

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-for file in small medium large; do
+for file in small medium smedium large; do
     wget "https://users.ugent.be/~rbmaerte/AD3/$file.txt.gz"
     gzip -d "$file.txt.gz"
 done
